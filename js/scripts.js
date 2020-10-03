efc7d11f0dfd7a66314a2213cc15cd945ff0836f
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2020-10-03 17:01:33 +0200

    js bootstrap locally

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#register_email").on('change', function () {
+        var email = $(this).val();
+        $.post("ajax_functions.php", { email: email }, function (data) {
+            $(".db-feedback").html(data);
+        });
+    });
+});
